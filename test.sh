fde9564ad52e9c1125ca3a7d37333a484cd178a3
Bruno Souza <56989493+brunosoouza@users.noreply.github.com>
2024-05-08 23:36:30 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,5 +10,6 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
+  -v $ROBOT_REQUIRIMENTS:/opt/robotframework/pip-requirements.txt:Z
   --user $(id -u):$(id -g) \
   $ROBOT_RUNNER_IMAGE
